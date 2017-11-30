2a38fe8fd34b5ec600c9bc338a4a1cb5e71d0bc3
Jan Chwiejczak <jchwiejczak@gmail.com>
2017-11-30 21:43:39 +0000

    Refactor data structure holding measurements

--- I2cSmBusBlockRead.py
+++ I2cSmBusBlockRead.py
@@ -1,42 +1,64 @@
 import smbus
+from collections import namedtuple
 from time import sleep
 
-bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+# 7 bit address (will be left shifted to add the read write bit)
+DEVICE_ADDRESS = 0x0A
+SLEEP_TIME = 1  # sec
+HEADERS = (
+    'time',
+    'v_a', 'i_a',
+    'v_b', 'i_b',
+    'temperature', 'light_intensity',
+    'error_a', 'error_b',
+)
 
-DEVICE_ADDRESS = 0x0A   #7 bit address (will be left shifted to add the read write bit)
-SLEEP_TIME = 1		#sec
+# A single measurement at given time
+Packet = namedtuple('Packet', HEADERS)
 
-# take bytes as list and return int - little endian (LSB on the left)
+
 def bytes_to_int(bytes):
-	x = 0
-	shift = 0    
-	for byte in bytes:
-        	x |= (byte << shift)
-		shift += 8
-	return x
+    """take bytes as list and return int - little endian (LSB on the left)"""
+    x = 0
+    shift = 0
+    for byte in bytes:
+        x |= (byte << shift)
+        shift += 8
+    return x
 
-# parse byte array to data
+
 def parse_lifetester_byte_array(bytes):
-	data = {
-	'time' : bytes_to_int(bytes[0:4]),
-	'v_a' : bytes_to_int(bytes[4:6]),
-	'i_a' : bytes_to_int(bytes[6:8]),
-	'v_b' : bytes_to_int(bytes[8:10]),
-	'i_b' : bytes_to_int(bytes[10:12]),
-	'temperature' : bytes_to_int(bytes[12:14]),
-	'light_intensity' : bytes_to_int(bytes[14:16]),
-	'error_a' : bytes[16],
-	'error_b' : bytes[17]
-	}
-	return data
+    """Return a packet representing a single point measurement."""
+    packet = Packet(
+        bytes_to_int(bytes[0:4]),
+        bytes_to_int(bytes[4:6]),
+        bytes_to_int(bytes[6:8]),
+        bytes_to_int(bytes[8:10]),
+        bytes_to_int(bytes[10:12]),
+        bytes_to_int(bytes[12:14]),
+        bytes_to_int(bytes[14:16]),
+        bytes[16],
+        bytes[17],
+    )
+    return packet
 
-def display_lifetester_data(data):
-	print "t%r: ch_A(v%r, i%r, err%r) ch_B(v%r, i%r, err%r) temp %r intensity %r" % (data['time'], data['v_a'], data['i_a'], data['error_a'], data['v_b'], data['i_b'], data['error_b'], data['temperature'], data['light_intensity'])
 
-while (True):
-	sleep(SLEEP_TIME)
-	block = bus.read_i2c_block_data(DEVICE_ADDRESS, 0, 18)
-	#print block
-	lifetester_data = parse_lifetester_byte_array(block)
-	display_lifetester_data(lifetester_data)
+def archive_lifetester_data(data):
+    with open('data.csv', w):
+        pass
 
+
+def main_loop():
+    """Main loop archiving and printing the data coming from I2c"""
+    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+    bus = smbus.SMBus(1)
+    while True:
+        sleep(SLEEP_TIME)
+        block = bus.read_i2c_block_data(DEVICE_ADDRESS, 0, 18)
+        lifetester_data = parse_lifetester_byte_array(block)
+        print(lifetester_data)
+        archive_lifetester_data(data)
+
+
+if __name__ == '__main__':
+    main_loop()
