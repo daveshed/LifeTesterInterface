d1d0231106437993fc53cf92440f368d18832e75
daveshed <davidkmohamad@gmail.com>
2017-11-30 23:02:07 +0000

    smashed it

--- I2cSmBusBlockRead.py
+++ I2cSmBusBlockRead.py
@@ -1,4 +1,7 @@
 import smbus
+import csv
+import time
+import sys
 from collections import namedtuple
 from time import sleep
 
@@ -43,21 +46,39 @@ def parse_lifetester_byte_array(bytes):
     return packet
 
 
-def archive_lifetester_data(data):
-    with open('data.csv', w):
-        pass
+def write_headers_row(filename):
+    with open(filename, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(HEADERS)
+
+
+def archive_lifetester_data(packet, filename):
+    with open(filename, 'ab') as archive:
+        writer = csv.writer(archive)
+        writer.writerow(packet)
+
+
+def get_unique_filename():
+    prefix = 'readings'
+    device_addr = '0x%.2X' % DEVICE_ADDRESS
+    date = time.strftime('%d_%b_%y-%H_%M_%S')
+    suffix = '.csv'
+    return '_'.join((prefix, device_addr, date, suffix))
 
 
 def main_loop():
     """Main loop archiving and printing the data coming from I2c"""
+    DEVICE_ADDRESS = int(sys.argv[1], base=16)
+    data_file = get_unique_filename()
+    write_headers_row(data_file)
     # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
     bus = smbus.SMBus(1)
     while True:
         sleep(SLEEP_TIME)
         block = bus.read_i2c_block_data(DEVICE_ADDRESS, 0, 18)
-        lifetester_data = parse_lifetester_byte_array(block)
-        print(lifetester_data)
-        archive_lifetester_data(data)
+        reading = parse_lifetester_byte_array(block)
+        print(reading)
+        archive_lifetester_data(reading, data_file)
 
 
 if __name__ == '__main__':
